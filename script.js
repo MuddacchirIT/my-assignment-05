a38006347bef93f26dbab9eb57a6d892cd60e5d7
unknown <protexbeer41@gmail.com>
2025-08-28 23:26:15 +0600

    clipboard added

--- script.js
+++ script.js
@@ -27,6 +27,9 @@ numberInc('button-copy', 'copy-score-board');
     // Call & History
     
     let callHistory = [];
+    let coins = 100;
+    const coinScoreBoard = document.getElementById("icon-score-board");
+    const copyScoreBoard = document.getElementById("copy-score-board");
 
     function calling(buttonId, numberId, nameId) {
         document.getElementById(buttonId).addEventListener('click', function(e) {
@@ -37,22 +40,34 @@ numberInc('button-copy', 'copy-score-board');
         const number = numberElement.innerText;
         const name = nameElement.innerText;
 
-        alert(`Calling ${number}`);
+        alert(`Calling ${name} ${number}`);
+
+         if (coins >= 20) {
+            coins -= 20;
+            coinScoreBoard.innerText = coins;
+        } else {
+            alert("You have no available Coins!");
+            return; 
+        }
 
         const now = new Date();
-        const timeString = now.toLocaleString();
+        const timeString = now.toLocaleString( 'en-US',{
+            hour: 'numeric', 
+            minute: 'numeric', 
+            second: 'numeric',
+            hour12: true
+        })
 
-    
         callHistory.push({ name: name, number: number, time: timeString });
 
         const callHistoryList = document.getElementById("call-history-list");
         const li = document.createElement("li");
         li.innerHTML = `
             <div class="flex flex-col">
-                <span class="font-semibold text-gray-800">${name}</span>
-                <span class="text-gray-600 text-xl">${number}</span>            
+                <span class="text-xl font-semibold text-gray-600">${name}</span>
+                <span class="text-gray-700 text-2xl font-bold">${number}</span>            
             </div>
-            <span class="text-gray-400 text-sm">${timeString}</span>
+            <span class="text-gray-900 text-lg font-semibold">${timeString}</span>
         `;
         li.classList.add(
             "bg-[#F5FFF6]", "p-5", "rounded-lg", "shadow", 
@@ -60,6 +75,32 @@ numberInc('button-copy', 'copy-score-board');
             "items-center", "my-3"
         );
         callHistoryList.appendChild(li);
+    });
+}
+
+function copying(buttonId, numberId, nameId, scoreBoardId) {
+    document.getElementById(buttonId).addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const numberElement = document.getElementById(numberId);
+        const nameElement = document.getElementById(nameId);
+        const number = numberElement.innerText;
+        const name = nameElement.innerText;
+        alert(`Calling ${name} ${number}`);
+
+        navigator.clipboard.writeText(`${name} - ${number}`)
+        .then(() => {
+            alert(`${name} ${number} copied to clipboard!`);
+        })
+        .catch(err => {
+            alert('Failed to copy!');
+            console.error(err);
+        });
+
+        const point = 1;
+        const availdPoint = parseInt(document.getElementById(scoreBoardId).innerText) || 0;
+        document.getElementById(scoreBoardId).innerText = availdPoint + point;
+        return
     });
 }
 calling("btn-national-emer", "call-emer", "name-national-emer");
@@ -71,3 +112,13 @@ calling("btn-anti", "call-anti", "name-anti");
 calling("btn-ele", "call-ele", "name-ele");
 calling("btn-brac", "call-brac", "name-brac");
 calling("btn-rail", "call-rail", "name-rail");
+
+copying("btn-national-emer-copy", "call-emer", "name-national-emer","copy-score-board");
+copying("btn-police-copy", "call-police", "name-police","copy-score-board");
+copying("btn-fire-copy", "call-fire", "name-fire", "copy-score-board");
+copying("btn-ambulance-copy", "call-ambulance", "name-ambulance","copy-score-board");
+copying("btn-women-copy", "call-women", "name-women","copy-score-board");
+copying("btn-anti-copy", "call-anti", "name-anti","copy-score-board");
+copying("btn-ele-copy", "call-ele", "name-ele","copy-score-board");
+copying("btn-brac-copy", "call-brac", "name-brac","copy-score-board");
+copying("btn-rail-copy", "call-rail", "name-rail","copy-score-board");
